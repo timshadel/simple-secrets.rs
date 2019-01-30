8cc36a1857df62e96df0f356889acdaf899f7842
Cameron Bytheway <bytheway.cameron@gmail.com>
2019-01-30 12:48:29 -0700

    fix keyring builds on linux

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,21 +1,16 @@
-
 //
 // Used modules
 //
 
-#[macro_use] extern crate failure;
+#[macro_use]
+extern crate failure;
 
 use exitfailure::ExitFailure;
 use failure::Error;
-use keyring::Keyring;
-use serde_json::Value;
+use serde_json::Value as JsonValue;
 use simple_secrets::Sender;
-use std::env;
-use std::fs::File;
 use std::io::prelude::*;
-use std::path::PathBuf;
 use structopt::StructOpt;
-
 
 //
 // App commands and options
@@ -24,7 +19,6 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "secrets", author = "")]
 struct App {
-
     /// Environment variable name containing the 256-bit key value in hex
     #[structopt(short = "e", long = "env", default_value = "SECRET_KEY")]
     env_name: String,
@@ -37,92 +31,118 @@ struct App {
     #[structopt(short = "a", long = "account")]
     key_account: Option<String>,
 
+    /// Operate in raw mode
+    #[structopt(short = "r", long = "raw")]
+    raw: bool,
+
     #[structopt(subcommand)]
     cmd: Command,
 }
 
 #[derive(Debug, StructOpt)]
 enum Command {
-
     /// Encrypt a JSON file to a websafe string
     #[structopt(name = "encrypt", author = "")]
     Encrypt {
         /// JSON input file to encrypt
-        #[structopt(parse(from_os_str))]
-        json_file: PathBuf,
+        #[structopt(name = "INPUT")]
+        input: String,
     },
 
     /// Decrypt a websafe string into a JSON object
     #[structopt(name = "decrypt", author = "")]
     Decrypt {
         /// Text file to decrypt
-        #[structopt(parse(from_os_str))]
-        text_file: PathBuf
-    }
-
+        #[structopt(name = "INPUT")]
+        input: String,
+    },
 }
-
 
 fn main() -> Result<(), ExitFailure> {
     let app = App::from_args();
-    let mut result = create_sender_from_env(app.env_name);
-    if let (Some(key_name), Some(service_name)) = (app.key_account, app.key_service) {
-        result = create_sender_from_keychain(service_name, key_name);
-    }
-    let sender = result?;
+    let sender = create_sender_from_keychain(&app)
+        .unwrap_or_else(|| create_sender_from_env(&app.env_name))?;
     match app.cmd {
-        Command::Encrypt { json_file } => {
-            let json = read_json(json_file)?;
-            println!("{}", sender.pack(&json)?);
-        },
-        Command::Decrypt { text_file } => {
-            let text = read_text(text_file)?;
-            let value: Value = sender.unpack(text)?;
-            println!("{}", serde_json::to_string_pretty(&value)?);
+        Command::Encrypt { input } => {
+            if app.raw {
+                let mut bytes = read_bytes(input)?;
+                println!("{}", sender.pack_raw(&mut bytes)?);
+            } else {
+                let json = read_json(input)?;
+                println!("{}", sender.pack(&json)?);
+            }
+        }
+        Command::Decrypt { input } => {
+            let text = read_text(input)?;
+            let text = text.trim();
+            if app.raw {
+                let result = sender.unpack_raw(text)?;
+                std::io::stdout().write(&result)?;
+            } else {
+                let json: JsonValue = sender.unpack(text)?;
+                println!("{}", serde_json::to_string_pretty(&json)?);
+            }
         }
     }
     Ok(())
 }
 
-
 //
 // Helpers
 //
 
-fn create_sender_from_env(name: String) -> Result<Sender, Error> {
-    let val = env::var(&name);
-    match val {
+fn create_sender_from_env(name: &str) -> Result<Sender, Error> {
+    use std::env::{var, VarError};
+    match var(name) {
         Ok(value) => Ok(Sender::new(value)?),
-        Err(err) => match err {
-            env::VarError::NotPresent =>
-                Err(AppError::MissingKeyName { name: name }.into()),
-            env::VarError::NotUnicode(val) =>
-                Err(AppError::InvalidKeyValue {
-                    value: val.to_string_lossy().to_string(),
-                    name: name
-                }.into())
+        Err(VarError::NotPresent) => Err(AppError::MissingKeyName {
+            name: name.to_owned(),
         }
+        .into()),
+        Err(VarError::NotUnicode(val)) => Err(AppError::InvalidKeyValue {
+            value: val.to_string_lossy().to_string(),
+            name: name.to_owned(),
+        }
+        .into()),
     }
 }
 
-fn create_sender_from_keychain(service: String, account: String) -> Result<Sender, Error> {
-    let keyring = Keyring::new(&service, &account);
-    let password = keyring.get_password()?;
-    Ok(Sender::new(password)?)
+fn create_sender_from_keychain(app: &App) -> Option<Result<Sender, Error>> {
+    let service = app.key_service.as_ref()?;
+    let account = app.key_account.as_ref()?;
+    Some(
+        keyring::Keyring::new(service, account)
+            .get_password()
+            .map_err(|e| AppError::KeyringError(e.to_string()).into())
+            .and_then(|password| Ok(Sender::new(password)?)),
+    )
 }
 
-fn read_json(path: PathBuf) -> Result<Value, Error> {
+fn read_json(path: String) -> Result<JsonValue, Error> {
     let contents = read_text(path)?;
     Ok(serde_json::from_str(&contents)?)
 }
 
-fn read_text(path: PathBuf) -> Result<String, Error> {
-    let mut file = File::open(path)?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-    Ok(contents)
+fn read_text(path: String) -> Result<String, Error> {
+    let bytes = read_bytes(path)?;
+    Ok(String::from_utf8(bytes)?)
 }
 
+fn read_bytes(path: String) -> Result<Vec<u8>, Error> {
+    if path == "-" {
+        let mut buffer = vec![];
+        std::io::stdin().read_to_end(&mut buffer)?;
+        return Ok(buffer);
+    }
+
+    let file_path = std::path::Path::new(&path);
+    if file_path.exists() {
+        return std::fs::read(file_path).map_err(|e| e.into());
+    }
+
+    // Try to decode the literal string passed
+    return Ok(path.into());
+}
 
 //
 // Errors
@@ -130,13 +150,18 @@ fn read_text(path: PathBuf) -> Result<String, Error> {
 
 #[derive(Debug, Fail)]
 enum AppError {
-    #[fail(display = "The key is missing. Expected 256-bit hex value in environment variable {}.", name)]
-    MissingKeyName {
-        name: String,
-    },
-    #[fail(display = "The key is invalid. Expected 256-bit hex value, but found '{}' in {}.", value, name)]
-    InvalidKeyValue {
-        value: String,
-        name: String
-    },
+    #[fail(
+        display = "The key is missing. Expected 256-bit hex value in environment variable {}.",
+        name
+    )]
+    MissingKeyName { name: String },
+
+    #[fail(
+        display = "The key is invalid. Expected 256-bit hex value, but found '{}' in {}.",
+        value, name
+    )]
+    InvalidKeyValue { value: String, name: String },
+
+    #[fail(display = "{}", 0)]
+    KeyringError(String),
 }
